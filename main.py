dd01a32e42b0750f89d090ec2646318fd0c680a0
rafli024 <muhammadrafli2460@gmail.com>
2022-08-09 14:26:20 +0700

    post to delete keys

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 # Scola Redis Manager
-# A system for managing redis server for scola LMS.
+# A system for managing r server for scola LMS.
 # __author__ = "Muhammad Rafli"
 # __copyright__ = "Copyright 2022, PT. Inovasi Edukasi Bangsa"
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from redis import Redis
 from waitress import serve
 from dotenv import load_dotenv
@@ -19,7 +19,7 @@ app_port = os.getenv('APP_PORT')
 
 app = Flask(__name__)
 
-redis = Redis(host=redis_host, port=redis_port, decode_responses=True)
+r = Redis(host=redis_host, port=redis_port, decode_responses=True)
 
 @app.get("/")
 def home(): 
@@ -28,19 +28,15 @@ def home():
 @app.get("/list")
 def get_list_keys():
        result = []
-       for key in redis.scan_iter("*-settingdata"):
+       for key in r.scan_iter("*"):
               result.append(key)
        return jsonify(results = result)
 
 @app.route("/delete/<keys>", methods=['POST'])
 def delete_keys(keys):
-    delete_keys = redis.delete(keys)
-    if methods == 'POST':
-        redis.delete(keys)
-        return { "status": True, "message": keys+" has been deleted."}
-    else:
-        return { "status": False, "message": keys+ "not found."}
-
+    if request.method == 'POST':
+        r.delete(keys)
+        return {'status': True, 'message': 'keys '+keys+' has been deleted'}
 
 if __name__ == '__main__':
     print("\033c", end="")
